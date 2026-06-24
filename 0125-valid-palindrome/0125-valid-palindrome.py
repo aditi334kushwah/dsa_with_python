056085df0815452e03e206bdb4a3d8d159aa334c
aditi <aaditikkushwah12530@gmail.com>
2026-06-24 15:37:20 +0530

    Time: 3 ms (98.99%), Space: 19.6 MB (77.27%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -1,19 +1,9 @@
-class Solution(object):
-    def isPalindrome(self, s):
-        
-        i = 0
-        j = len(s)-1
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        cleaned = ""
 
-        while i<j:
-            
-            while i < j and not s[i].isalnum():
-                i+=1
-            while i<j and not s[j].isalnum():
-                j-=1
-            
-            if s[i].lower() != s[j].lower() :
-                return False 
-            i+=1
-            j-=1
-            
-        return True
+        for ch in s:
+            if ch.isalnum():
+                cleaned += ch.lower()
+
+        return cleaned == cleaned[::-1]
